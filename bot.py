54048389ed8abcdbd91e2d29ed3244a2ce9e0b30
miki275 <mil.cepo@gmail.com>
2026-05-07 16:19:26 +0200

    Refactor Telegram bot and improve message formatting

--- bot.py
+++ bot.py
@@ -1,61 +1,75 @@
-from flask import Flask
-import threading
-import os
 import requests
 import time
 from bs4 import BeautifulSoup
-#fix bot
+
+# Telegram bot podac
 TOKEN = "8679506052:AAGSx0N3zOrajN70WyXcVqHvkzHFGaChvQE"
 CHAT_ID = "8203943962"
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Bot radi!"
+# Dionice koje pratiš
 WATCHLIST = ["JDRN", "ATPL", "CKML"]
 
+# Sprema već viđene objave
 seen = set()
+
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        }
+    )
+
 
 def fetch_eho():
-    url = "https://eho.zse.hr/obavijesti-izdavatelja/"
+    url = "https://eho.zse.hr/obavijesti-izdavatelja"
+
     r = requests.get(url)
+
     soup = BeautifulSoup(r.text, "html.parser")
 
     results = []
+
     for a in soup.find_all("a"):
         text = a.get_text(strip=True)
         link = a.get("href")
+
         if text and len(text) > 20:
             results.append((text, link))
 
     return results
-while   True:
+
+
+while True:
     try:
         posts = fetch_eho()
 
         for title, link in posts:
+
             if title in seen:
                 continue
 
             seen.add(title)
 
             for ticker in WATCHLIST:
+
                 if ticker in title:
-                    msg = f"🚨 {ticker}\n{title}\n{link}"
+
+                    msg = (
+                        f"🚨 {ticker}\n\n"
+                        f"{title}\n\n"
+                        f"{link}"
+                    )
+
                     send(msg)
 
         time.sleep(60)
 
     except Exception as e:
         print(e)
-        time.sleep(60)
-    def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
 
-threading.Thread(target=run_web).start()    
-        
+        time.sleep(60)
